f0b337d6bb9a7437a225a0da5ffabd61241c8865
pascal-maker <pascal-musa@hotmail.com>
2025-01-26 21:32:55 +0100

    yess

--- #exercise 07.py
+++
@@ -1,7 +0,0 @@
-#exercise 07
-x = int(input(" Enter a start value:"))
-step_size = int(input("enter a positive step size:"))
-numbers_printed = int(input("How many numbers need to be printed?"))
-def print_list_numbers (x,step_size,numbers_printed):
- print(f" The numbers you are looking for: {print_list_numbers}")   
-for x in range(10,34,6):
